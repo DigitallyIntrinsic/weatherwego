624f688f54da9ee0567940ac39714bb3e6fb24d0
DigitallyIntrinsic <139934612+DigitallyIntrinsic@users.noreply.github.com>
2023-09-15 22:45:00 -0500

    adding search function

--- assets/js/script.js
+++ assets/js/script.js
@@ -109,7 +109,7 @@ $(document).ready(function () {
             alert("Could not get data")
         });
 
-        //Function to get 5-day forecast and UV index and put them on page
+        // Function to get 5-day forecast and UV index and put them on page
         function getUV(lat, lon) {
 
 
@@ -118,7 +118,7 @@ $(document).ready(function () {
                 method: "GET",
             }).then(function (response) {
 
-                //code to determine UV index severity
+                // code to determine UV index severity
                 let uvIndex = response.current.uvi;
                 $("#uv-index").text("UV Index:" + " " + uvIndex);
                 if (uvIndex >= 8) {
@@ -131,19 +131,21 @@ $(document).ready(function () {
                 let cityHigh = response.daily[0].temp.max;
                 $("#high").text("Expected high (F): " + " " + cityHigh);
 
-                //forecast temp variables
+                // forecast temp variables
                 let day1temp = response.daily[1].temp.max;
                 let day2temp = response.daily[2].temp.max;
                 let day3temp = response.daily[3].temp.max;
                 let day4temp = response.daily[4].temp.max;
                 let day5temp = response.daily[5].temp.max;
-                //forecast humidity variables
+
+                // forecast humidity variables
                 let day1hum = response.daily[1].humidity;
                 let day2hum = response.daily[2].humidity;
                 let day3hum = response.daily[3].humidity;
                 let day4hum = response.daily[4].humidity;
                 let day5hum = response.daily[5].humidity;
-                //forecast weather icon variables
+
+                // forecast weather icon variables
                 let icon1 = response.daily[1].weather[0].icon;
                 let icon2 = response.daily[2].weather[0].icon;
                 let icon3 = response.daily[3].weather[0].icon;
@@ -155,28 +157,36 @@ $(document).ready(function () {
                 $("#temp3").text("Temp(F):" + " " + day3temp.toFixed(1));
                 $("#temp4").text("Temp(F):" + " " + day4temp.toFixed(1));
                 $("#temp5").text("Temp(F):" + " " + day5temp.toFixed(1));
-        
+
                 $("#hum1").text("Hum:" + " " + day1hum + "%");
                 $("#hum2").text("Hum:" + " " + day2hum + "%");
                 $("#hum3").text("Hum:" + " " + day3hum + "%");
                 $("#hum4").text("Hum:" + " " + day4hum + "%");
                 $("#hum5").text("Hum:" + " " + day5hum + "%");
-        
+
                 $("#icon1").html(
-                  `<img src="http://openweathermap.org/img/wn/${icon1}@2x.png">`
+                    `<img src="http://openweathermap.org/img/wn/${icon1}@2x.png">`
                 );
                 $("#icon2").html(
-                  `<img src="http://openweathermap.org/img/wn/${icon2}@2x.png">`
+                    `<img src="http://openweathermap.org/img/wn/${icon2}@2x.png">`
                 );
                 $("#icon3").html(
-                  `<img src="http://openweathermap.org/img/wn/${icon3}@2x.png">`
+                    `<img src="http://openweathermap.org/img/wn/${icon3}@2x.png">`
                 );
                 $("#icon4").html(
-                  `<img src="http://openweathermap.org/img/wn/${icon4}@2x.png">`
+                    `<img src="http://openweathermap.org/img/wn/${icon4}@2x.png">`
                 );
                 $("#icon5").html(
-                  `<img src="http://openweathermap.org/img/wn/${icon5}@2x.png">`
+                    `<img src="http://openweathermap.org/img/wn/${icon5}@2x.png">`
                 );
-              });
-            }
-          }
+            });
+        }
+    }
+
+    // function to render recently searched cities to page
+    function listCities() {
+        $("#cityList").text("");
+        cities.forEach((city) => {
+            $("#cityList").prepend("<tr><td>" + city + "</td></tr>");
+        });
+    }
