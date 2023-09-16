f3d45e6eb4a8ddf6878995a5c5a344513dca8453
DigitallyIntrinsic <139934612+DigitallyIntrinsic@users.noreply.github.com>
2023-09-15 22:21:54 -0500

    continuing function

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,10 @@ $(document).ready(function () {
 
     //Pulls the current date
     let NowMoment = moment().format("l");
+
+      //adds days to moment for forecast
+  let day1 = moment().add(1, "days").format("l");
+  let day2 = moment().add(2, "days").format("l");
+  let day3 = moment().add(3, "days").format("l");
+  let day4 = moment().add(4, "days").format("l");
+  let day5 = moment().add(5, "days").format("l");
